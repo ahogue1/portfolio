cd1c19a656748f43bde84d4362ca39ae200d2ad6
Amy Hogue <hogue.a@gmail.com>
2018-09-13 23:26:18 -0500

    Add links to other pages

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -5,13 +5,19 @@ class BlogController < ApplicationController
       select { |f| f.ends_with?('.html.erb') }.
       map { |f|
         id = f.chomp('.html.erb')
-        { id: id, title: id[3..-1].capitalize }
+        { id: id, title: remove_order(id) }
       }.reverse
   end
 
   def show
-    @title = "Danger 2.0 | #{params[:id].capitalize}"
+    @title = "Danger 2.0 | #{remove_order(params[:id])}"
 
     render "posts/#{params[:id]}"
+  end
+
+  private
+
+  def remove_order(slug)
+    slug[3..-1].titleize
   end
 end
